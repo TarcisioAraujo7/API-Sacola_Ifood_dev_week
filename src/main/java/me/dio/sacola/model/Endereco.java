17f2a3674f279896ed1fe2280d0c8ee34394ea08
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-11-01 19:40:18 -0300

    remoção de imports desnecessários

--- src/main/java/me/dio/sacola/model/Endereco.java
+++ src/main/java/me/dio/sacola/model/Endereco.java
@@ -1,13 +1,11 @@
 package me.dio.sacola.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
+++ src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
@@ -5,7 +5,6 @@ import me.dio.sacola.enumeration.FormaPagamento;
 import me.dio.sacola.model.Item;
 import me.dio.sacola.model.Restaurante;
 import me.dio.sacola.model.Sacola;
-import me.dio.sacola.repository.ItemRepository;
 import me.dio.sacola.repository.ProdutoRepository;
 import me.dio.sacola.repository.SacolaRepository;
 import me.dio.sacola.resource.dto.ItemDto;
@@ -22,8 +21,6 @@ public class SacolaServiceImpl implements SacolaService {
 
     private final SacolaRepository sacolaRepository;
     private final ProdutoRepository produtoRepository;
-
-    private final ItemRepository itemRepository;
 
     @Override
     public Item incluirItemNaSacola(ItemDto itemDto) {
